afac1c533785f5ac544bd3ba05a53a830be43314
pe2ya <32875661+pe2ya@users.noreply.github.com>
2022-02-11 10:24:13 +0100

    Prepare for User menu

---
+++ Menu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    public class Menu
+    {
+        private string caption { get; set; }
+        private List<MenuItem> menuItems = new List<MenuItem>();
+
+        public Menu(string caption)
+        {
+            this.caption = caption;
+        }
+        public void Show()
+        {
+            Console.WriteLine(caption);
+            for (int i = 0; i < menuItems.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {menuItems[i]}");
+            }
+        }
+
+        public MenuItem Selection()
+        {
+            string input = Console.ReadLine();
+            int index;
+            if (!int.TryParse(input, out index))
+            {
+                Console.Error.WriteLine($"Invalid input '{input}'");
+                return null;
+            }
+            else {
+                index = Convert.ToInt32(input) - 1;
+                if (index < 0 || index >= menuItems.Count)
+                {
+                    Console.Error.WriteLine($"Index {input} is not valid input");
+                    return null;
+                }
+            }
+            return menuItems[index];
+        }
+
+        public MenuItem Execute()
+        {
+            MenuItem item = null;
+            do
+            {
+                Show();
+                item = Selection();
+            } while (item == null);
+
+            return item;
+        }
+
+        public void Add(MenuItem menuItem)
+        {
+            this.menuItems.Add(menuItem);
+        }
+    }
+}
---
+++ MenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MenuItem
+    {
+        private string description;
+        private Action action;
+
+        public MenuItem(string popis, Action akce)
+        {
+            this.description = popis;
+            this.action = akce;
+        }
+
+        public override string ToString()
+        {
+            return description;
+        }
+
+        public void Execute()
+        {
+            action();
+        }
+    }
+}
